b8a9a24293a6c7b7e94f32d84ba865edd276207b
luanmilagre <luanmilagrereal@gmail.com>
2025-04-08 20:27:05 -0400

    Add files via upload

---
+++ Delete.py
@@ -0,0 +1,28 @@
+# Importar a biblioteca
+import mysql.connector
+
+# Conexão com o MySQL
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Luanmilagre.123",
+    database="teste"
+)
+cursor = conexao.cursor()
+
+
+nome_produto = "chocolate"
+
+# Comando SQL para deletar dados
+comando = "DELETE FROM vendas WHERE nome_produto = %s"
+
+# Dados que serão inseridos no comando
+dados = (nome_produto)
+
+# Executa o comando
+cursor.execute(comando, dados)
+conexao.commit()  # Confirma as alterações no banco de dados
+
+# Fecha a conexão
+cursor.close()
+conexao.close()
